5dd2bdf04c0e99aa72274bc7857322c3a70f6c61
ShRehm <shrehm@hotmail.com>
2018-04-24 09:16:34 +0000

    Hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     
     def hello
-    render html: "hello, world!"
+    render html: "hello, world world world!"
     end
     
     
