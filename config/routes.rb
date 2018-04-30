b2607c4386192d23997883bf1ee70a9bdbcc8e17
ShRehm <shrehm@hotmail.com>
2018-04-30 13:55:53 +0000

    Finish Toy App

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
- root 'application#index'
+ #root 'application#index'
+ root 'users#index'
 
 end
